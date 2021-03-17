29da4e3c323ec3c8a711438c72581fba54818ce5
Aritra Roy <78724027+AritraRoyBMSCE@users.noreply.github.com>
2021-03-17 09:02:37 +0530

    Update CopyStringandLength.c

--- CopyStringandLength.c
+++ CopyStringandLength.c
@@ -5,16 +5,16 @@
 #define SI 1000
 void copyandlength(char str1[]){
     char str2[SI]; int i=0,c=0;
-    while(str1[i]!=0){
+    while(str1[i]!='\0'){
         str2[i]=str1[i];
         i++;
     }
     str2[i]='\0';
     printf("The copied string is : %s \n", str2);
-    while(str1[c]!=0){
+    while(str2[c]!='\0'){
         c++;
     }
-    str1[c]='\0';
+    str2[c]='\0';
     printf("The length of the string is = %d", c);
 }
 int main(){
